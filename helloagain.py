f6805b8755ccf196fc06c95c6deee33d0754e869
laneystrange <laneyd@gmail.com>
2025-11-17 16:17:49 -0500

    adding a non-main function

--- helloagain.py
+++ helloagain.py
@@ -2,6 +2,8 @@ def main() -> None:
     username = input("What is your name?")
     print(f"Hello {username}!")
     
+def cel_to_fahr(c : float) -> float:
+    return c * 9/5 + 32
 
 if __name__ == "__main__":
     main()
