05409b2e565be07103419118538300374aa8a69b
laneystrange <laneyd@gmail.com>
2026-01-09 09:18:58 -0500

    adding lecture 3 starter code for testing

---
+++ test_lec3_start.py
@@ -0,0 +1,20 @@
+'''
+    CS2100
+    Spring 2026
+    Sample code from class - unittest for our functions
+'''
+
+import unittest
+# import module and functions to test
+
+class TestRunners(unittest.TestCase):
+    ''' test the functions defined in lec3_v2_dictionaries '''
+    def test_first_method(self):
+        pass
+        # self.assertEqual(functioncall, expected output)
+        # self.assertEqual(functioncall, expected output)
+        # self.assertEqual(functioncall, expected output)
+
+
+if __name__ == "__main__":
+    unittest.main()
